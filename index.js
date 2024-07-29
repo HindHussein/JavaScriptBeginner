498ca9204e0bf6fbc79e8760a305f7783d976624
Hind Hussein <hind.suleimanh@gmail.com>
2024-07-30 01:10:08 +0300

    Basics Commit

---
+++ index.js
@@ -0,0 +1,64 @@
+// console.log(`Hello`);
+// console.log(`I like pizza`);
+// window.alert(`okay whats going on`);
+
+// document.getElementById("myH1").textContent = `Hind`;
+// document.getElementById("myP").textContent = `I like Pizza`;
+
+// let age = 21;
+
+// console.log(typeof age);
+// console.log(`You are ${age} years old`);
+
+// let email = "HindHu1205@gmail.com"
+// console.log(typeof email);
+
+/*let fullName = "Hind Hussein";
+let age = 21;
+let typeOfEducation = "Graduate"; 
+
+document.getElementById("p1").textContent = `Your Full Name is ${fullName}`;
+document.getElementById("p2").textContent = `You are ${age} Years Old`;
+
+if(typeOfEducation.match("Student")) { 
+    document.getElementById("p3").textContent = `You are Currently a Student`;
+} else if (typeOfEducation.match("Graduate")) {
+    document.getElementById("p3").textContent = "You have Graduated";
+} else if (typeOfEducation.match("Unemployed")) {
+    document.getElementById("p3").textContent = "You are Currently Unemployed";
+} else {
+    document.getElementById("p3").textContent = "You're Working!";
+}
+
+
+let result = 12 % 5 + 8 / 2;
+console.log(result); */
+
+/*
+let username;
+
+document.getElementById("myButton").onclick = function(){
+    username = document.getElementById("myText").value;
+    document.getElementById("myLabel").style.display = "none";
+    document.getElementById("myH1").textContent = `Welcome ${username}`;
+    document.getElementById("myText").style.display = "none";
+    document.getElementById("myButton").style.display = "none";
+    console.log(username);
+}
+    */
+
+
+
+/*let age = window.prompt("Whats your Age?");
+age = Number(age); //making it a number since its a string by default
+age+= 1;
+document.getElementById("myH1").textContent = `You are ${age} Years Old`;
+console.log(age);
+
+*/
+
+
+
+
+
+
