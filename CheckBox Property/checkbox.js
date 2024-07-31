57fb7cab83da4817f9ea6cd8016af324e7e71678
Hind Hussein <hind.suleimanh@gmail.com>
2024-07-31 17:27:06 +0300

    CheckBox: Ensuring User Writes their Name

--- CheckBox Property/checkbox.js
+++ CheckBox Property/checkbox.js
@@ -16,46 +16,51 @@ let myH3;
 
 submitButton.onclick = function() {
 
-    nameInput.style.display = "none";
-    graduateCheckBox.style.display = "none";
-    graduateLabel.style.display = "none";
-    studentCheckBox.style.display = "none";
-    studentLabel.style.display = "none";
-    unemployedCheckBox.style.display = "none";
-    unemployedLabel.style.display = "none";
-    submitButton.style.display = "none";
-    myLabel.style.display = "none";
-    mySecondLabel.style.display = "none";
+    if(nameInput.value.trim() !== ""){
+        nameInput.style.display = "none";
+        graduateCheckBox.style.display = "none";
+        graduateLabel.style.display = "none";
+        studentCheckBox.style.display = "none";
+        studentLabel.style.display = "none";
+        unemployedCheckBox.style.display = "none";
+        unemployedLabel.style.display = "none";
+        submitButton.style.display = "none";
+        myLabel.style.display = "none";
+        mySecondLabel.style.display = "none";
 
+        if(graduateCheckBox.checked){
+            myH3 = `Welcome, ${nameInput.value}! Congratulations on Graduating`
+            console.log(myH3);
+            document.getElementById("myH3").textContent = myH3;
+        }
+        else if(studentCheckBox.checked){
+            myH3 = `Welcome, ${nameInput.value}! You are Currently a Student`
+            document.getElementById("myH3").textContent = myH3;
+        }
+        else if(unemployedCheckBox.checked){
+            myH3 = `Welcome, ${nameInput.value}! You are Currently Unemployed:(`
+            document.getElementById("myH3").textContent = myH3;
+        }
 
-    if(graduateCheckBox.checked){
-        myH3 = `Welcome, ${nameInput.value}! Congratulations on Graduating`
-        console.log(myH3);
-        document.getElementById("myH3").textContent = myH3;
+        else {
+            nameInput.style.display = "inline";
+            graduateCheckBox.style.display = "inline";
+            graduateLabel.style.display = "inline";
+            studentCheckBox.style.display = "inline";
+            studentLabel.style.display = "inline";
+            unemployedCheckBox.style.display = "inline";
+            unemployedLabel.style.display = "inline";
+            submitButton.style.display = "inline";
+            myLabel.style.display = "inline";
+            mySecondLabel.style.display = "inline";
+            
+            myH3 = `Make Sure You've Chosen a Position`
+            document.getElementById("myH3").textContent = myH3;
+        }
     }
-    else if(studentCheckBox.checked){
-        myH3 = `Welcome, ${nameInput.value}! You are Currently a Student`
-        document.getElementById("myH3").textContent = myH3;
-    }
-    else if(unemployedCheckBox.checked){
-        myH3 = `Welcome, ${nameInput.value}! You are Currently Unemployed:(`
-        document.getElementById("myH3").textContent = myH3;
-    }
-
     else {
-        nameInput.style.display = "inline";
-        graduateCheckBox.style.display = "inline";
-        graduateLabel.style.display = "inline";
-        studentCheckBox.style.display = "inline";
-        studentLabel.style.display = "inline";
-        unemployedCheckBox.style.display = "inline";
-        unemployedLabel.style.display = "inline";
-        submitButton.style.display = "inline";
-        myLabel.style.display = "inline";
-        mySecondLabel.style.display = "inline";
-        
-        myH3 = `Make Sure You're Inputting Information`
-        document.getElementById("myH3").textContent = myH3;
+        myH3 = `Make Sure You've Written Your Name`
+            document.getElementById("myH3").textContent = myH3;
     }
 }
 
